a492e1850b8845194da1ccd6a0b2c578cd66325c
Milan Horvatovic <milan.horvatovic@gmail.com>
2019-06-18 00:08:11 +0200

    Update generator to support more combinations

--- StrongifyGenerator/StrongifyGenerator/main.swift
+++ StrongifyGenerator/StrongifyGenerator/main.swift
@@ -8,6 +8,57 @@
 
 import Foundation
 
+enum InputType {
+    
+    case none
+    case error
+    case `default`
+    
+    var fileName: String? {
+        get {
+            switch self {
+            case .none:
+                return .none
+            case .error:
+                return "Throwable"
+            case .default:
+                return .none
+            }
+        }
+    }
+    
+}
+
+enum ThrowableType {
+    
+    case none
+    case rethrow
+    
+    var throwable: Bool {
+        get {
+            switch self {
+            case .none:
+                return false
+            case .rethrow:
+                return true
+            }
+        }
+    }
+    
+    
+    var fileName: String? {
+        get {
+            switch self {
+            case .none:
+                return .none
+            case .rethrow:
+                return "Rethrow"
+            }
+        }
+    }
+    
+}
+
 enum ReturnType {
     
     case void
@@ -44,67 +95,84 @@ enum ReturnType {
         }
     }
     
+    var fileName: String? {
+        get {
+            switch self {
+            case .void:
+                return .none
+            case .type:
+                return "Return"
+            case .optional:
+                return "ReturnOptional"
+            }
+        }
+    }
+    
 }
 
 enum GenerationType {
     
-    case base(throwable: Bool, returnType: ReturnType)
-    case fatalError(throwable: Bool, returnType: ReturnType)
-    
+    case base(input: InputType, throwable: ThrowableType, returnType: ReturnType)
+    case fatalError(input: InputType, throwable: ThrowableType, returnType: ReturnType)
     
     var fatalError: Bool {
         switch self {
-        case .base(_, _): return false
-        case .fatalError(_, _): return true
-        }
-    }
-    
-    var throwable: Bool {
-        switch self {
-        case .base(let throwable, _): return throwable
-        case .fatalError(let throwable, _): return throwable
+        case .base(_, _, _):
+            return false
+        case .fatalError(_, _, _):
+            return true
         }
     }
     
     var `return`: ReturnType {
         switch self {
-        case .base(_, let returnType): return returnType
-        case .fatalError(_, let returnType): return returnType
+        case .base(_, _, let returnType): return returnType
+        case .fatalError(_, _, let returnType): return returnType
         }
     }
     
     var fileName: String {
+        var path: [String?] = ["Strongify"]
         switch self {
-        case .base(let throwable, let returnType):
-            switch returnType {
-            case .void: return "Strongify" + (throwable ? "Throws" : "") + ".swift"
-            case .type: return "StrongifyReturn" + (throwable ? "Throws" : "") + ".swift"
-            case .optional: return "StrongifyReturnOptional" + (throwable ? "Throws" : "") + ".swift"
-            }
-        case .fatalError(let throwable, let returnType):
-            switch returnType {
-            case .void: return "StrongifyFatalError" + (throwable ? "Throws" : "") + ".swift"
-            case .type: return "StrongifyFatalErrorReturn" + (throwable ? "Throws" : "") + ".swift"
-            case .optional: return "StrongifyFatalErrorReturnOptional" + (throwable ? "Throws" : "") + ".swift"
-            }
+        case .base(let input, let throwable, let `return`):
+            path.append(input.fileName)
+            path.append(throwable.fileName)
+            path.append(`return`.fileName)
+        case .fatalError(let input, let throwable, let `return`):
+            path.append("FatalError")
+            path.append(input.fileName)
+            path.append(throwable.fileName)
+            path.append(`return`.fileName)
         }
+        path.append(".swift")
+        return path
+            .compactMap({ (value: String?) -> String? in
+                return value
+            })
+            .joined()
     }
     
 }
 
 let combinations: [GenerationType] = [
-    .base(throwable: false, returnType: .void),
-    .base(throwable: false, returnType: .type),
-    .base(throwable: false, returnType: .optional),
-    .base(throwable: true, returnType: .void),
-    .base(throwable: true, returnType: .type),
-    .base(throwable: true, returnType: .optional),
-    .fatalError(throwable: false, returnType: .void),
-    .fatalError(throwable: false, returnType: .type),
-    .fatalError(throwable: false, returnType: .optional),
-    .fatalError(throwable: true, returnType: .void),
-    .fatalError(throwable: true, returnType: .type),
-    .fatalError(throwable: true, returnType: .optional)
+    .base(input: .none, throwable: .none, returnType: .void),
+    .base(input: .none, throwable: .rethrow, returnType: .void),
+    
+    .base(input: .error, throwable: .rethrow, returnType: .void),
+    .base(input: .error, throwable: .rethrow, returnType: .type),
+    .base(input: .error, throwable: .rethrow, returnType: .optional),
+    
+    .base(input: .default, throwable: .none, returnType: .type),
+    .base(input: .default, throwable: .rethrow, returnType: .type),
+    .base(input: .default, throwable: .none, returnType: .optional),
+    .base(input: .default, throwable: .rethrow, returnType: .optional),
+    
+    .fatalError(input: .none, throwable: .none, returnType: .void),
+    .fatalError(input: .none, throwable: .rethrow, returnType: .void),
+    .fatalError(input: .none, throwable: .none, returnType: .type),
+    .fatalError(input: .none, throwable: .rethrow, returnType: .type),
+    .fatalError(input: .none, throwable: .none, returnType: .optional),
+    .fatalError(input: .none, throwable: .rethrow, returnType: .optional)
 ]
 
 func generate(contexts: UInt8 = 1, arguments: UInt8 = 0, type: GenerationType) -> String {
@@ -116,15 +184,32 @@ func generate(contexts: UInt8 = 1, arguments: UInt8 = 0, type: GenerationType) -
     
     body += "/**\n Creates a closure that automatically deals with weak-strong dance\n\n"
     body += " - Parameters:\n"
-    if contextsPlaceholders.count > 0 { body += contextsPlaceholders.enumerated().map({ (index: Int, item: String) -> String in return "  - \(item)\(index + 1): Any \(item) object to weakify and strongify" }).joined(separator: "\n") + "\n" }
+    if contextsPlaceholders.count > 0 {
+        body += contextsPlaceholders.enumerated()
+            .map({ (index: Int, item: String) -> String in
+                return "  - \(item)\(index + 1): Any \(item) object to weakify and strongify"
+            })
+            .joined(separator: "\n")
+            + "\n"
+    }
     switch type.fatalError {
     case true: body += "  - message: Message for `fatalError` when one of the context doesn't exist\n"
-    case false: if let returnDocumentation: String = type.`return`.documentation, returnDocumentation.count > 0 { body += "  - return: \(returnDocumentation)\n" }
+    case false:
+        let returnDocumentation: String = type.`return`.documentation
+        if returnDocumentation.count > 0 {
+            body += "  - return: \(returnDocumentation)\n"
+        }
     }
     body += "  - closure: Closure to execute instead of the original one\n"
     body += "*/\n"
-    let contextsFuncDesc: [String] = contextsPlaceholders.enumerated().map({ (index: Int, item: String) -> String in return "\(item.prefix(1).uppercased() + item.dropFirst())\(index + 1): AnyObject" })
-    let argumentsFuncDesc: [String] = argumentsPlaceholders.enumerated().map({ (index: Int, item: String) -> String in return "\(item.prefix(1).uppercased() + item.dropFirst())\(index + 1)" })
+    let contextsFuncDesc: [String] = contextsPlaceholders.enumerated()
+        .map({ (index: Int, item: String) -> String in
+            return "\(item.prefix(1).uppercased() + item.dropFirst())\(index + 1): AnyObject"
+        })
+    let argumentsFuncDesc: [String] = argumentsPlaceholders.enumerated()
+        .map({ (index: Int, item: String) -> String in
+            return "\(item.prefix(1).uppercased() + item.dropFirst())\(index + 1)"
+        })
     
     let contextsFuncArgsDesc: [String] = contextsPlaceholders.enumerated().map({ (index: Int, item: String) -> String in
         let content: String = "\(item)\(index + 1): \(item.prefix(1).uppercased() + item.dropFirst())\(index + 1)?"
@@ -134,66 +219,186 @@ func generate(contexts: UInt8 = 1, arguments: UInt8 = 0, type: GenerationType) -
         }
     })
     
-    let contextsParamDesc: [String] = contextsPlaceholders.enumerated().map({ (index: Int, item: String) -> String in return "\(item.prefix(1).uppercased() + item.dropFirst())\(index + 1)" })
-    let argumentsParamDesc: [String] = argumentsPlaceholders.enumerated().map({ (index: Int, item: String) -> String in return "\(item.prefix(1).uppercased() + item.dropFirst())\(index + 1)" })
+    let contextsParamDesc: [String] = contextsPlaceholders.enumerated()
+        .map({ (index: Int, item: String) -> String in
+            return "\(item.prefix(1).uppercased() + item.dropFirst())\(index + 1)"
+        })
+    let argumentsParamDesc: [String] = argumentsPlaceholders.enumerated()
+        .map({ (index: Int, item: String) -> String in
+            return "\(item.prefix(1).uppercased() + item.dropFirst())\(index + 1)"
+        })
     
-    let contextsClosureDesc: [String] = contextsPlaceholders.enumerated().map({ (index: Int, item: String) -> String in return "weak \(item)\(index + 1)" })
-    let argumentsClosureDesc: [String] = argumentsPlaceholders.enumerated().map({ (index: Int, item: String) -> String in return "\(item)\(index + 1): \(item.prefix(1).uppercased() + item.dropFirst())\(index + 1)" })
+    let contextsClosureDesc: [String] = contextsPlaceholders.enumerated()
+        .map({ (index: Int, item: String) -> String in
+            return "weak \(item)\(index + 1)"
+        })
+    let argumentsClosureDesc: [String] = argumentsPlaceholders.enumerated()
+        .map({ (index: Int, item: String) -> String in
+            return "\(item)\(index + 1): \(item.prefix(1).uppercased() + item.dropFirst())\(index + 1)"
+        })
     
-    let contextsLetDesc: [String] = contextsPlaceholders.enumerated().map({ (index: Int, item: String) -> String in return "let strong\(item.prefix(1).uppercased() + item.dropFirst())\(index + 1): \(item.prefix(1).uppercased() + item.dropFirst())\(index + 1) = \(item)\(index + 1)" })
-    let contextsArgsDesc: [String] = contextsPlaceholders.enumerated().map({ (index: Int, item: String) -> String in return "strong\(item.prefix(1).uppercased() + item.dropFirst())\(index + 1)" })
-    let argumentsArgsDesc: [String] = argumentsPlaceholders.enumerated().map({ (index: Int, item: String) -> String in return "\(item)\(index + 1)" })
+    let contextsLetDesc: [String] = contextsPlaceholders.enumerated()
+        .map({ (index: Int, item: String) -> String in
+            return "let strong\(item.prefix(1).uppercased() + item.dropFirst())\(index + 1): \(item.prefix(1).uppercased() + item.dropFirst())\(index + 1) = \(item)\(index + 1)"
+        })
+    let contextsArgsDesc: [String] = contextsPlaceholders.enumerated()
+        .map({ (index: Int, item: String) -> String in
+            return "strong\(item.prefix(1).uppercased() + item.dropFirst())\(index + 1)"
+        })
+    let argumentsArgsDesc: [String] = argumentsPlaceholders.enumerated()
+        .map({ (index: Int, item: String) -> String in
+            return "\(item)\(index + 1)"
+        })
     
     //  definition
     body += "public func strongify<"
-    if contextsFuncDesc.count > 0 { body += contextsFuncDesc.joined(separator: ", ") }
-    if argumentsFuncDesc.count > 0 { body += ", " + argumentsFuncDesc.joined(separator: ", ") }
+    if contextsFuncDesc.count > 0 {
+        body += contextsFuncDesc.joined(separator: ", ")
+    }
+    if argumentsFuncDesc.count > 0 {
+        body += ", " + argumentsFuncDesc.joined(separator: ", ")
+    }
     switch type.`return` {
-    case .void: break
-    case .type, .optional: body += ", " + type.`return`.funcGenDesc
+    case .void:
+        break
+    case .type, .optional:
+        body += ", " + type.`return`.funcGenDesc
     }
     body += ">("
-    if contextsFuncArgsDesc.count > 0 { body += contextsFuncArgsDesc.joined(separator: ", ") }
-    switch type.fatalError {
-    case true: body += ", message: String"
-    case false:
-        switch type.return {
-        case .void: break
-        case .type, .optional: body += ", `return`: " + type.`return`.funcDesc
+    if contextsFuncArgsDesc.count > 0 {
+        body += contextsFuncArgsDesc.joined(separator: ", ")
+    }
+    switch type {
+    case .base(let input, _, _):
+        switch input {
+        case .none:
+            break
+        case .error:
+            body += ", `throws` error: Error"
+        case .default:
+            body += ", `return`: " + type.`return`.funcDesc
         }
+    case .fatalError(_, _, _):
+        body += ", message: String"
     }
     body += ", closure: @escaping ("
-    if contextsParamDesc.count > 0 { body += contextsParamDesc.joined(separator: ", ") }
-    if argumentsParamDesc.count > 0 { body += ", " + argumentsParamDesc.joined(separator: ", ") }
-	//	NOTE: each strongify function doesn't need to be marked as rethrows because return statement is only closure block itself
-    //body += ")" + (type.throwable ? " throws" : "") + " -> " + type.`return`.funcDesc + ")" + (type.throwable ? " rethrows" : "") + " -> (("
-	body += ")" + (type.throwable ? " throws" : "") + " -> " + type.`return`.funcDesc + ")" + " -> (("
-    if argumentsParamDesc.count > 0 { body += argumentsParamDesc.joined(separator: ", ") }
-    body += ")" + (type.throwable ? " throws" : "") + " -> " + type.`return`.funcDesc + ") {\n"
-    //  body
-    body += "\treturn { ["
-    if contextsClosureDesc.count > 0 { body += contextsClosureDesc.joined(separator: ", ") }
-    body += "] ("
-    if argumentsClosureDesc.count > 0 { body += argumentsClosureDesc.joined(separator: ", ") }
-    body += ")"
-    if let returnDesc: String = type.`return`.funcReturnDesc, returnDesc.count > 0 { body += " -> " + returnDesc }
-    body += " in\n"
-    body += "\t\tguard "
-    if contextsLetDesc.count > 0 { body += contextsLetDesc.joined(separator: ", ") }
-    switch type.fatalError {
-    case true: body += " else { fatalError(message) }\n"
-    case false:
-        switch type.return {
-        case .void: body += " else { return }\n"
-        case .type, .optional: body += " else { return `return` }\n"
+    if contextsParamDesc.count > 0 {
+        body += contextsParamDesc.joined(separator: ", ")
+    }
+    if argumentsParamDesc.count > 0 {
+        body += ", " + argumentsParamDesc.joined(separator: ", ")
+    }
+    switch type {
+    case .base(_, let throwable, _):
+        switch throwable {
+        case .none:
+            body += ") -> " + type.`return`.funcDesc + ")" + " -> (("
+        case .rethrow:
+        body += ") throws -> " + type.`return`.funcDesc + ")" + " -> (("
+        }
+    case .fatalError(_, let throwable, _):
+        switch throwable {
+        case .none:
+            body += ") -> " + type.`return`.funcDesc + ")" + " -> (("
+        case .rethrow:
+            body += ") throws -> " + type.`return`.funcDesc + ")" + " -> (("
         }
     }
-    switch type.return {
-    case .void: body += "\t\t" + (type.throwable ? "try" : "") + " closure("
-    case .type, .optional: body += "\t\treturn " + (type.throwable ? "try" : "") + " closure("
+    if argumentsParamDesc.count > 0 {
+        body += argumentsParamDesc.joined(separator: ", ")
     }
-    if contextsArgsDesc.count > 0 { body += contextsArgsDesc.joined(separator: ", ") }
-    if argumentsArgsDesc.count > 0 { body += ", " + argumentsArgsDesc.joined(separator: ", ") }
+    switch type {
+    case .base(_, let throwable, _):
+        switch throwable {
+        case .none:
+            body += ") -> " + type.`return`.funcDesc + ") {\n"
+        case .rethrow:
+            body += ") throws -> " + type.`return`.funcDesc + ") {\n"
+        }
+    case .fatalError(_, let throwable, _):
+        switch throwable {
+        case .none:
+            body += ") -> " + type.`return`.funcDesc + ") {\n"
+        case .rethrow:
+            body += ") throws -> " + type.`return`.funcDesc + ") {\n"
+        }
+    }
+    //  body
+    body += "\treturn { ["
+    if contextsClosureDesc.count > 0 {
+        body += contextsClosureDesc.joined(separator: ", ")
+    }
+    body += "] ("
+    if argumentsClosureDesc.count > 0 {
+        body += argumentsClosureDesc.joined(separator: ", ")
+    }
+    body += ")"
+    do {
+        let returnDesc: String = type.`return`.funcReturnDesc
+        if returnDesc.count > 0 {
+            body += " -> " + returnDesc
+        }
+    }
+    body += " in\n"
+    body += "\t\tguard "
+    if contextsLetDesc.count > 0 {
+        body += contextsLetDesc.joined(separator: ", ")
+    }
+    switch type {
+    case .base(let input, _, _):
+        switch input {
+        case .none:
+            body += " else { return }\n"
+        case .error:
+            body += " else { throw error }\n"
+        case .default:
+            body += " else { return `return` }\n"
+        }
+    case .fatalError(_, _, _):
+        body += " else { fatalError(message) }\n"
+    }
+    switch type {
+    case .base(_, let throwable, let returnType):
+        switch returnType {
+        case .void:
+            switch throwable {
+            case .none:
+                body += "\t\tclosure("
+            case .rethrow:
+                body += "\t\ttry closure("
+            }
+        case .type, .optional:
+            switch throwable {
+            case .none:
+                body += "\t\treturn closure("
+            case .rethrow:
+                body += "\t\treturn try closure("
+            }
+        }
+    case .fatalError(_, let throwable, let returnType):
+        switch returnType {
+        case .void:
+            switch throwable {
+            case .none:
+                body += "\t\tclosure("
+            case .rethrow:
+                body += "\t\ttry closure("
+            }
+        case .type, .optional:
+            switch throwable {
+            case .none:
+                body += "\t\treturn closure("
+            case .rethrow:
+                body += "\t\treturn try closure("
+            }
+        }
+    }
+    if contextsArgsDesc.count > 0 {
+        body += contextsArgsDesc.joined(separator: ", ")
+    }
+    if argumentsArgsDesc.count > 0 {
+        body += ", " + argumentsArgsDesc.joined(separator: ", ")
+    }
     body += ")\n"
     body += "\t}\n"
     
